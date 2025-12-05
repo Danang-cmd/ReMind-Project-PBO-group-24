1670434e08d38698443c5b833880073cf51d96bb
bintang310803 <136469152+bintang310803@users.noreply.github.com>
2025-12-05 14:43:25 +0700

    Update Card.java

--- src/model/Card.java
+++ src/model/Card.java
@@ -1,17 +1,38 @@
 package model;
 
 import javax.swing.JButton;
+import util.Clickable;
 
-public class Card {
-    public int id;            
+public class Card implements Clickable {
+
+    public int id;
     public JButton button;
-    public boolean matched;   
-    public int position;      
+    public boolean matched;
+    public int position;
+
+    private Runnable clickAction;
 
     public Card(int id, JButton btn, int position) {
         this.id = id;
         this.button = btn;
-        this.matched = false;
         this.position = position;
+        this.matched = false;
+    }
+
+    public void setClickAction(Runnable action) {
+        this.clickAction = action;
+        button.addActionListener(e -> onClick());
+    }
+
+    @Override
+    public void onClick() {
+        if (clickAction != null && isEnabled()) {
+            clickAction.run();
+        }
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return !matched;
     }
 }
